296bb8eb942bdb19ab4ce22efb3ff19954203c56
Vincent <2039855@carrefour.cegepvicto.ca>
2021-11-07 17:20:37 -0500

    Modification du script de la creation de la base de donnees

--- ressources/creationdb.sql
+++ ressources/creationdb.sql
@@ -1,26 +1,16 @@
 CREATE DATABASE LVDELH;
 
-
 USE LVDELH;
-
 
 CREATE TABLE `hero` (
   `id_hero` int PRIMARY KEY,
-  `page_id` int
+  `chapitre_id` int
 );
 
 CREATE TABLE `chapitre` (
   `id_chapitre` int PRIMARY KEY,
   `livre_id` int,
   `numero_chapitre` int,
-  `nom_chapitre` varchar(50)
-);
-
-CREATE TABLE `page` (
-  `id_page` int PRIMARY KEY,
-  `livre_id` int,
-  `numero_page` int,
-  `chapitre_id` int,
   `texte` text
 );
 
@@ -31,10 +21,8 @@ CREATE TABLE `livre` (
   `nom_auteur` varchar(255)
 );
 
-ALTER TABLE `page` ADD FOREIGN KEY (`chapitre_id`) REFERENCES `chapitre` (`id_chapitre`);
-
-ALTER TABLE `hero` ADD FOREIGN KEY (`page_id`) REFERENCES `page` (`id_page`);
-
 ALTER TABLE `chapitre` ADD FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id_livre`);
 
-ALTER TABLE `page` ADD FOREIGN KEY (`livre_id`) REFERENCES `livre` (`id_livre`);
+ALTER TABLE `hero` ADD FOREIGN KEY (`chapitre_id`) REFERENCES `chapitre` (`id_chapitre`);
+
+

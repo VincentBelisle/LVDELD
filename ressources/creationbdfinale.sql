55af93e46ac2643bce42cb2349bc7e37e0ab73e1
felix <michaud07@outlook.com>
2021-11-20 00:37:44 -0500

    reessaie de push les dernieres versions? gros fuck on espere que ca va marcher

--- ressources/creationbdfinale.sql
+++ ressources/creationbdfinale.sql
@@ -88,7 +88,7 @@ INSERT INTO hero (nom) VALUES("Michel");
 
 # Création de l'usager pour la bd.
 #CREATE USER 'hero'@'localhost' IDENTIFIED BY 'hero';
-GRANT SELECT, UPDATE, INSERT ON ldvelh.* TO 'hero'@'localhost';
+GRANT SELECT, UPDATE, EXECUTE, INSERT ON ldvelh.* TO 'hero'@'localhost';
 
 DELIMITER $$
 CREATE FUNCTION dee() RETURNS INT NO SQL
@@ -133,7 +133,7 @@ DROP PROCEDURE IF EXISTS afficher_chapitre$$
 CREATE PROCEDURE afficher_chapitre(IN _numero_chapitre INT)
 BEGIN
 
-SELECT texte FROM chapitre WHERE numero_chapitre = _numero_chapitre;
+SELECT texte FROM chapitre WHERE chapitre.id = _numero_chapitre;
 
 END $$
 DELIMITER ;
@@ -172,11 +172,25 @@ CREATE PROCEDURE enregistrer_donnees(IN id_sac INT, IN objet_1 INT, IN objet_2 I
 					vie = vie 
                     WHERE fiche_personnage.id = id_personnage; 
                 
-            END $$
+            END $$fiche_personnage
 DELIMITER ;
 
 
 
-CALL afficher_chapitre(1);
+CALL afficher_chapitre(0);
 
+SELECT chapitre_id FROM session WHERE id = 4; 
+SELECT * FROM hero; 
+SELECT * FROM chapitre; 
+SELECT * FROM session; 
+SELECT * FROM fiche_personnage;
+INSERT INTO session (hero_id, chapitre_id, nom) VALUES (1,2,'test'); 
+INSERT INTO sac_a_dos (objet_1) VALUES ('test1'); 
+INSERT INTO aventure (id) VALUES (1); 
+INSERT INTO  fiche_personnage (id, session_id, vie, endurance, sac_a_dos_id, aventure_id) VALUES (1, 4,10, 30, 1,1);
+
+UPDATE session
+SET chapitre_id = 1
+WHERE id = 4; 
+SELECT * FROM chapitre WHERE id = 141;
 
